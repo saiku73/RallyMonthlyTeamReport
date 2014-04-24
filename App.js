e22a13ec389084d7a1ecf11ff2f4de8c52cdd0fc
OpenShift guest <sai.ramani@gmail.com>
2014-04-24 12:23:39 -0400

    added month pulldown

--- App.js
+++ App.js
@@ -19,7 +19,43 @@ Ext.define('CustomApp', {
         
     },
     _loadMonthPulldown : function() {
-        
+         // Set up a model to use in our Store
+        Ext.define('Month', {
+            extend: 'Ext.data.Model',
+            fields: [
+                {name: 'name', type: 'string'},
+                {name: 'startDate',  type: 'string'},
+                {name: 'endDate',    type: 'string'},
+                {name: 'dateString', type: 'string'}   // 2014-01-01:2014-01-31
+            ]});
+
+        var myStore = Ext.create('Ext.data.Store', {
+            model: 'Month',
+            data: [
+                { 'name': 'Jan 1st, 2014', 'startDate': '2014-01-01', 'endDate': '2014-01-31' },
+                { 'name': 'Feb 1st, 2014', 'startDate': '2014-02-01', 'endDate': '2014-02-28' },
+                { 'name': 'Mar 1st, 2014', 'startDate': '2014-03-01', 'endDate': '2014-03-31' }
+                ]
+            });
+            
+        var monthCombobox = Ext.create('Rally.ui.combobox.ComboBox', {
+            itemId: "month-combobox", //we'll use this item id to get the user's selection
+            fieldLabel: "Month",
+            labelAlign: "right",
+            displayField: 'name',
+            valueField: 'startDate',
+            width: 300,
+            store: myStore,
+            listeners: {
+                select: this._onMonthSelected, //combo and records will be passed in automatically
+                scope : this //This is confusing ....it sets scope to the Ext object.
+            }
+        }
+        );
+        this.down('#filter-pulldown-container').add(monthCombobox);
+    },
+    _onMonthSelected: function(combo, records) {
+        console.log('selected month', records[0].get('startDate'), records[0].get('endDate'));
     },
     _loadTeamsPulldown : function() {
         var teamCombobox = Ext.create('Rally.ui.combobox.ComboBox', {
@@ -48,8 +84,6 @@ Ext.define('CustomApp', {
         this.down('#filter-pulldown-container').add(teamCombobox);
     },
     _onTeamSelected : function(combo, records) {
-        console.log(records[0] + ' team was selected'); 
-        console.log('team was selected', records[0], combo); 
         console.log('%s team was selected', records[0]); //Use this if you don't know what properties u want. It will pretty print in the console.
         console.log('%s team was selected', records[0].get('Name'));
         
